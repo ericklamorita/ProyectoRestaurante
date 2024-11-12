79a9401039c9a4ad250787ce43c6e661208f0a60
Jeff6669 <jeffrey.am.18@gmail.com>
2024-11-11 19:47:24 -0600

    Update Trabajador.java

--- src/proyectorestaurante/Trabajador.java
+++ src/proyectorestaurante/Trabajador.java
@@ -1,18 +1,24 @@
 package proyectorestaurante;
 
 public class Trabajador {
+    private int id;
     private String nombre;
     private String apellido1;
     private String apellido2;
 
-    public Trabajador(String nombre, String apellido1, String apellido2) {
+    public Trabajador(int id, String nombre, String apellido1, String apellido2) {
+        this.id = id;
         this.nombre = nombre;
         this.apellido1 = apellido1;
         this.apellido2 = apellido2;
     }
 
+    public int getId() {
+        return id;
+    }
+
     @Override
     public String toString() {
-        return nombre + " " + apellido1 + " " + apellido2;
+        return "ID: " + id + ", Nombre: " + nombre + " " + apellido1 + " " + apellido2;
     }
 }
